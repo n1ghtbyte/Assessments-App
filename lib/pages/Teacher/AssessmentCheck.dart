07e1b4ea5514fe378f9147d61775e1cc79b98f33
n1ghtbyte <tomasgc@disroot.org>
2022-07-11 18:34:39 +0100

    Grid list showing the students

--- lib/pages/Teacher/AssessmentCheck.dart
+++ lib/pages/Teacher/AssessmentCheck.dart
@@ -12,35 +12,62 @@ class AssessmentCheck extends StatefulWidget {
   State<AssessmentCheck> createState() => _AssessmentCheckState();
 }
 
+final CollectionReference _assess =
+    FirebaseFirestore.instance.collection('assessments');
+
 class _AssessmentCheckState extends State<AssessmentCheck> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Review'),
-        centerTitle: true,
-        backgroundColor: Color(0xff29d092),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: null,
-        backgroundColor: const Color(0xFF29D09E),
-        label: Text('All'),
-        icon: Icon(Icons.all_inclusive),
-      ),
-      body: GridView.count(
-        // Create a grid with 2 columns. If you change the scrollDirection to
-        // horizontal, this produces 2 rows.
-        crossAxisCount: 3,
-        // Generate 100 widgets that display their index in the List.
-        children: List.generate(12, (index) {
-          return Center(
-            child: Text(
-              'Item $index',
-              style: Theme.of(context).textTheme.headline5,
+    return FutureBuilder<DocumentSnapshot>(
+        future: _assess.doc(widget.passedAssessmentIdName).get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
+
+          if (!snapshot.hasData) {
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          print(data['Students'].keys);
+          var a = Map();
+
+          List<String> studentsList = data['Students'].keys.toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Review'),
+              centerTitle: true,
+              backgroundColor: Color(0xff29d092),
+            ),
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: null,
+              backgroundColor: const Color(0xFF29D09E),
+              label: Text('All'),
+              icon: Icon(Icons.all_inclusive),
+            ),
+            body: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 3,
+              // Generate 100 widgets that display their index in the List.
+              children: List.generate(studentsList.length, (index) {
+                return Center(
+                  child: Text(
+                    '${studentsList[index]}',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                );
+              }),
             ),
           );
-        }),
-      ),
-    );
+        });
   }
 }
