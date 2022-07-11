9289f48482ce21e55d236892c89452de9e824462
n1ghtbyte <tomasgc@disroot.org>
2022-07-11 17:48:00 +0100

    Review every student in the assessment doc

---
+++ lib/pages/Teacher/Classes/ReviewTheAssess.dart
@@ -0,0 +1,285 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:assessments_app/assets/group_radio_button.dart';
+
+class ReviewAssessment extends StatefulWidget {
+  final String passedAssessmentIdName;
+  const ReviewAssessment({Key? key, required this.passedAssessmentIdName})
+      : super(key: key);
+  @override
+  _ReviewAssessmentState createState() => _ReviewAssessmentState();
+}
+
+class _ReviewAssessmentState extends State<ReviewAssessment> {
+  String? currentUser = FirebaseAuth.instance.currentUser!.email;
+
+  Future<void> addGrade(String stud, Map<dynamic, Map<dynamic, dynamic>> comp,
+      var assessinfo, String _id) {
+    late CollectionReference assessments = FirebaseFirestore.instance
+        .collection('classes')
+        .doc(assessinfo['ClassId'].toString())
+        .collection("grading")
+        .doc(stud.toString())
+        .collection("grades");
+
+    // Call the user's CollectionReference to add a new assessment
+    return assessments.add({
+      'ClassName': assessinfo['ClassId'],
+      'Created': FieldValue.serverTimestamp(),
+      'Creator': currentUser,
+      'Type': "Formative",
+      'Competences': comp,
+      'AssessID': _id,
+    }).then((value) {
+      print(value.id);
+    });
+  }
+
+  String? number; //no radio button will be selected
+  Map<dynamic, Map<dynamic, dynamic>> _mapao = {};
+  Map<dynamic, dynamic> kek = Map();
+  Map<dynamic, dynamic> _mapinha = {};
+  Stream<QuerySnapshot> _stream =
+      FirebaseFirestore.instance.collection('Competences').snapshots();
+
+  final CollectionReference _assess =
+      FirebaseFirestore.instance.collection('assessments');
+
+  Future<void> updateAssessment(
+      String _docid, Map _alumni, var data, bool done) {
+    return _assess
+        .doc(_docid)
+        .update({})
+        .then((value) => print("Assessment Updated"))
+        .catchError((error) => print("Failed to update assessment: $error"));
+  }
+
+  List<String> textifier(List<dynamic>? x) {
+    return x!.cast<String>();
+  }
+
+  // Fetch the Assessment Invoice
+  Widget build(BuildContext context) {
+    return FutureBuilder<DocumentSnapshot>(
+        future: _assess.doc(widget.passedAssessmentIdName).get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
+
+          if (!snapshot.hasData) {
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          // Fetch all the Competences (not much anyway)
+          return StreamBuilder<QuerySnapshot>(
+              stream: _stream,
+              builder:
+                  (BuildContext context, AsyncSnapshot<QuerySnapshot> snp) {
+                if (snp.hasError) {
+                  return Text('Something went wrong');
+                }
+
+                if (snp.connectionState == ConnectionState.waiting) {
+                  return Container(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+                Map<String, List<dynamic>> _comps = {};
+                //  print("dados da snap" + snp.data!.docs.length.toString());
+                for (var i = 0; i < 11; i++) {
+                  Map<String, dynamic> foo =
+                      snp.data?.docs[i].data()! as Map<String, dynamic>;
+                  for (var f in foo.keys) {
+                    if (f.toString() != "Name") {
+                      _comps[f.toString()] = foo[f.toString()];
+                    }
+                  }
+                }
+                List studsToAssess = [];
+                Map studs = data['Students'];
+                var assesId = data['documentID'];
+                // Checks if the student is already assessed.
+                studs.forEach((key, value) {
+                  if (value == true) studsToAssess.add(key);
+                });
+                var currentStudent = studsToAssess[0];
+                var _indicators = [];
+                List<String> namesC = [];
+                List<String> allIndicators = [];
+
+                var namesComp = data['Competences'].keys;
+                for (var t in namesComp) {
+                  namesC.add(t);
+                }
+
+                for (var x in data['Competences'].values) {
+                  _indicators.add(x);
+                }
+
+                for (var i in _indicators) {
+                  for (var j in i) {
+                    allIndicators.add(j);
+                  }
+                }
+                return MaterialApp(
+                  home: Scaffold(
+                    appBar: AppBar(
+                      title: Text('Review Assessment'),
+                      centerTitle: true,
+                      backgroundColor: Color(0xFF29D09E),
+                    ),
+                    floatingActionButton: FloatingActionButton.extended(
+                      backgroundColor: const Color(0xFF29D09E),
+                      onPressed: () {
+                        for (var ent in _mapinha.keys) {
+                          _mapinha[ent] =
+                              _mapinha[ent].toString().substring(0, 1);
+                        }
+
+                        for (var skill in _mapao.keys) {
+                          _mapao[skill]?.updateAll(
+                              ((key, value) => value = value.substring(0, 1)));
+                        }
+                        studs[currentStudent] = true;
+                        addGrade(currentStudent, _mapao, data, assesId);
+                        bool x = true;
+                        for (var k in studs.values) {
+                          if (k == false) x = false;
+                        }
+                        updateAssessment(
+                            widget.passedAssessmentIdName, studs, data, x);
+                        if (x == true) {
+                          FirebaseFirestore.instance
+                              .collection('classes')
+                              .doc(data['ClassId'])
+                              .update({'prevAssess': FieldValue.increment(1)});
+                          Navigator.pop(context);
+                        } else {
+                          print(studs);
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      super.widget));
+                        }
+                      },
+                      icon: Icon(Icons.skip_next),
+                      label: Text('Next'),
+                    ),
+                    body: SafeArea(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                "Student: $currentStudent\nClass:${data['ClassName']}\nAssessment: ${data['currentNumber']}#",
+                                style: TextStyle(fontSize: 26),
+                              ),
+                            ),
+                            for (var k in namesC)
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Divider(
+                                        height: 20,
+                                        thickness: 4,
+                                      ),
+                                      SizedBox(
+                                        height: 40,
+                                      ),
+                                      Text(
+                                        k,
+                                        style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      for (String x in List<String>.from(
+                                          data['Competences'][k] as List))
+                                        Padding(
+                                          padding: EdgeInsets.all(16.0),
+                                          child: Column(children: <Widget>[
+                                            Divider(
+                                              height: 4,
+                                            ),
+                                            Text(
+                                              x,
+                                              style: TextStyle(fontSize: 18),
+                                            ),
+                                            SizedBox(
+                                              height: 12,
+                                            ),
+                                            RadioGroup<String>.builder(
+                                              spacebetween: 65.0,
+                                              direction: Axis.vertical,
+                                              groupValue: _mapinha[x.toString()]
+                                                  .toString(),
+                                              horizontalAlignment:
+                                                  MainAxisAlignment.start,
+                                              onChanged: (value) {
+                                                setState(
+                                                  () {
+                                                    _mapinha[x.toString()] =
+                                                        value.toString();
+                                                    if (_mapao[k.toString()] !=
+                                                        null) {
+                                                      _mapao[k.toString()]![
+                                                              x.toString()] =
+                                                          value.toString();
+                                                    } else {
+                                                      _mapao[k.toString()] =
+                                                          Map();
+                                                    }
+                                                    _mapao[k.toString()]![
+                                                            x.toString()] =
+                                                        value.toString();
+
+                                                    print(_mapao.toString());
+                                                  },
+                                                );
+                                              },
+                                              activeColor: Color(0xFF29D09E),
+                                              items: textifier(_comps[x]),
+                                              textStyle: TextStyle(
+                                                fontSize: 14,
+                                              ),
+                                              itemBuilder: (item) =>
+                                                  RadioButtonBuilder(
+                                                      item.toString(),
+                                                      textPosition:
+                                                          RadioButtonTextPosition
+                                                              .left),
+                                            ),
+                                          ]),
+                                        )
+                                    ]),
+                              ),
+                            SizedBox(
+                              height: 32,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              });
+        });
+  }
+}
