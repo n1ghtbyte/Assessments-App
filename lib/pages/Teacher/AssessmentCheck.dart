f237a8252d1bf49c92b5785e2169423612698fdb
n1ghtbyte <tomasgc@disroot.org>
2022-07-14 13:49:38 +0100

    removed trash

--- lib/pages/Teacher/AssessmentCheck.dart
+++ lib/pages/Teacher/AssessmentCheck.dart
@@ -1,6 +1,5 @@
 import 'package:assessments_app/pages/Teacher/AssessReviewSolo.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AssessmentCheck extends StatefulWidget {
