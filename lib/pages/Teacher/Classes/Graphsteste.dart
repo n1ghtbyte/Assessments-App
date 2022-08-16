db818504f0c257b9455ea5a5718c0ed160c4d94a
n1ghtbyte <tomasgc@disroot.org>
2022-08-16 18:58:48 +0100

    commented out

--- lib/pages/Teacher/Classes/Graphsteste.dart
+++ lib/pages/Teacher/Classes/Graphsteste.dart
@@ -1,69 +1,69 @@
-import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+// import 'package:flutter/material.dart';
+// import 'package:syncfusion_flutter_charts/charts.dart';
 
-void main() {
-  return runApp(_ChartApp());
-}
+// void main() {
+//   return runApp(_ChartApp());
+// }
 
-class _ChartApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: _MyHomePage(),
-    );
-  }
-}
+// class _ChartApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: _MyHomePage(),
+//     );
+//   }
+// }
 
-class _MyHomePage extends StatefulWidget {
-  // ignore: prefer_const_constructors_in_immutables
-  _MyHomePage({Key? key}) : super(key: key);
+// class _MyHomePage extends StatefulWidget {
+//   // ignore: prefer_const_constructors_in_immutables
+//   _MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<_MyHomePage> {
-  late List<_ChartData> data;
-  late TooltipBehavior _tooltip;
+// class _MyHomePageState extends State<_MyHomePage> {
+//   late List<_ChartData> data;
+//   late TooltipBehavior _tooltip;
 
-  @override
-  void initState() {
-    data = [
-      _ChartData('CHN', 12),
-      _ChartData('GER', 15),
-      _ChartData('RUS', 30),
-      _ChartData('BRZ', 6.4),
-      _ChartData('IND', 14)
-    ];
-    _tooltip = TooltipBehavior(enable: true);
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     data = [
+//       _ChartData('CHN', 12),
+//       _ChartData('GER', 15),
+//       _ChartData('RUS', 30),
+//       _ChartData('BRZ', 6.4),
+//       _ChartData('IND', 14)
+//     ];
+//     _tooltip = TooltipBehavior(enable: true);
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Syncfusion Flutter chart'),
-        ),
-        body: SfCartesianChart(
-            primaryXAxis: CategoryAxis(),
-            primaryYAxis: NumericAxis(minimum: 0, maximum: 40, interval: 10),
-            tooltipBehavior: _tooltip,
-            series: <ChartSeries<_ChartData, String>>[
-              BarSeries<_ChartData, String>(
-                  dataSource: data,
-                  xValueMapper: (_ChartData data, _) => data.x,
-                  yValueMapper: (_ChartData data, _) => data.y,
-                  name: 'Gold',
-                  color: Color.fromRGBO(8, 142, 255, 1))
-            ]));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Syncfusion Flutter chart'),
+//         ),
+//         body: SfCartesianChart(
+//             primaryXAxis: CategoryAxis(),
+//             primaryYAxis: NumericAxis(minimum: 0, maximum: 40, interval: 10),
+//             tooltipBehavior: _tooltip,
+//             series: <ChartSeries<_ChartData, String>>[
+//               BarSeries<_ChartData, String>(
+//                   dataSource: data,
+//                   xValueMapper: (_ChartData data, _) => data.x,
+//                   yValueMapper: (_ChartData data, _) => data.y,
+//                   name: 'Gold',
+//                   color: Color.fromRGBO(8, 142, 255, 1))
+//             ]));
+//   }
+// }
 
-class _ChartData {
-  _ChartData(this.x, this.y);
+// class _ChartData {
+//   _ChartData(this.x, this.y);
 
-  final String x;
-  final double y;
-}
+//   final String x;
+//   final double y;
+// }
