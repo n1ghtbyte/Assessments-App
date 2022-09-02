febb7a92d8af71c3a74d251868062d4134fb930f
n1ghtbyte <tomasgc@disroot.org>
2022-09-02 15:30:59 +0100

    TODO ADD PALLETE BIGGER

--- lib/pages/Teacher/Classes/AstaGraphs.dart
+++ lib/pages/Teacher/Classes/AstaGraphs.dart
@@ -83,10 +83,6 @@ class _AstaGraphsState extends State<AstaGraphs> {
     Color(0xff33691e),
   ];
 
-  // static Map<String, Color> _compColour = {
-  //   "Writing Skills": Color.fromARGB(255, 172, 55, 137),
-  //   "Project Management": Color.fromARGB(255, 166, 229, 42),
-  // };
   int indicatorToHash(String indicator) {
     int _sum = 0;
 
