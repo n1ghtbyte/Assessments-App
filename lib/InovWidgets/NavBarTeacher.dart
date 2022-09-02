dfbd736a02674ef76256996368a91f8d5562d7d1
n1ghtbyte <tomasgc@disroot.org>
2022-09-02 18:28:28 +0100

    Promise to getPictureProfile TODO null value

--- lib/InovWidgets/NavBarTeacher.dart
+++ lib/InovWidgets/NavBarTeacher.dart
@@ -5,6 +5,7 @@ import 'package:assessments_app/pages/Teacher/Skills/SkillsPage.dart';
 import 'package:assessments_app/pages/Teacher/TeacherProfile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class NavBarTeacher extends StatefulWidget {
   const NavBarTeacher({Key? key}) : super(key: key);
@@ -16,145 +17,148 @@ class NavBarTeacher extends StatefulWidget {
 class _NavBarTeacherState extends State<NavBarTeacher> {
   String? currentUser = FirebaseAuth.instance.currentUser!.email;
   final _firebaseAuth = FirebaseAuth.instance;
+  String path = "rip";
+
+  // final gsReference = FirebaseStorage.instance
+  //     .refFromURL(
+  //         "gs://assessments-app-o3.appspot.com/images/profilepic/catcute.jpg")
+  //     .getDownloadURL();
+
+  Future<String> function() async {
+    final storage = FirebaseStorage.instanceFor(
+        bucket: "gs://assessments-app-o3.appspot.com");
+    String k = await storage
+        .ref()
+        // .child('images/profilepic/${currentUser.toString()}')
+        .child('images/profilepic/pepepiramid.jpg')
+        .getDownloadURL();
+    print("DS");
+    return k;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text('Teacher'),
-            accountEmail: Text(currentUser!),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfDUf1sal3kszOWelEchcmmhjaZwbYCwa-Iw&usqp=CAU',
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+    return FutureBuilder(
+      future: function(),
+      builder: (BuildContext context, AsyncSnapshot<String> text) {
+        if (text.hasData) {
+          return Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text('Teacher'),
+                  accountEmail: Text(currentUser!),
+                  currentAccountPicture: CircleAvatar(
+                    child: ClipOval(
+                      child: Image.network(
+                        text.data!,
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(color: Color(0xFF29D09E)),
                 ),
-              ),
-            ),
-            decoration: BoxDecoration(color: Color(0xFF29D09E)),
-          ),
-          // ListTile(
-          //   leading: Icon(Icons.assessment),
-          //   title: const Text('Assessments'),
-          //   onTap: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(
-          //           builder: (context) => AssessmentsTeacherPage()),
-          //     );
-          //   },
-          // ),
-          ListTile(
-            leading: Icon(Icons.school),
-            title: const Text('Classes'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ClassesPage()),
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.menu_book),
-            title: const Text('Competences'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SkillsPage()),
-              );
-            },
-          ),
-          const SizedBox(height: 16),
-          Divider(
-            thickness: 1,
-            height: 1,
-          ),
-          const SizedBox(height: 16),
-          ListTile(
-            leading: Icon(Icons.account_box),
-            title: const Text('Account'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => TeacherProfile()),
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: const Text('Settings'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsPage()),
-              );
-            },
-          ),
-          ListTile(
-              leading: Icon(Icons.logout),
-              title: const Text('Log out'),
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                            title: Text("Logout"),
-                            content: Text('Log out now?'),
-                            actions: [
-                              TextButton(
-                                style: ButtonStyle(
-                                  foregroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Colors.blue),
-                                ),
-                                onPressed: () {
+                ListTile(
+                  leading: Icon(Icons.school),
+                  title: const Text('Classes'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ClassesPage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.menu_book),
+                  title: const Text('Competences'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SkillsPage()),
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+                Divider(
+                  thickness: 1,
+                  height: 1,
+                ),
+                const SizedBox(height: 16),
+                ListTile(
+                  leading: Icon(Icons.account_box),
+                  title: const Text('Account'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => TeacherProfile()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: const Text('Log out'),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: Text("Logout"),
+                        content: Text('Log out now?'),
+                        actions: [
+                          TextButton(
+                            style: ButtonStyle(
+                              foregroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.blue),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true).pop();
+                            },
+                            child: Text('CANCEL'),
+                          ),
+                          TextButton(
+                            style: ButtonStyle(
+                              foregroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.blue),
+                            ),
+                            onPressed: () async {
+                              await _firebaseAuth.signOut().then(
+                                (user) {
                                   Navigator.of(context, rootNavigator: true)
                                       .pop();
-                                },
-                                child: Text('CANCEL'),
-                              ),
-                              TextButton(
-                                style: ButtonStyle(
-                                  foregroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Colors.blue),
-                                ),
-                                // onPressed: () async {
-                                //   await _googleSignIn.signOut();
-                                //   setState(() {});
-                                // },
-                                // Navigator.push(
-                                //   context,
-                                //   MaterialPageRoute(builder: (context) => QuitPage()),
-
-                                onPressed: () async {
-                                  await _firebaseAuth.signOut().then(
-                                    (user) {
-                                      Navigator.of(context, rootNavigator: true)
-                                          .pop();
-                                      Navigator.of(context).pushReplacement(
-                                        MaterialPageRoute(
-                                          builder: (context) => LoginScreen(),
-                                        ),
-                                      );
-                                    },
+                                  Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                      builder: (context) => LoginScreen(),
+                                    ),
                                   );
                                 },
-                                child: Text('LOGOUT'),
-                              )
-                            ]));
-              }),
-        ],
-      ),
+                              );
+                            },
+                            child: Text('LOGOUT'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
-  }
-}
-
-void selectedItem(BuildContext context, int index) {
-  switch (index) {
-    case 0:
   }
 }
