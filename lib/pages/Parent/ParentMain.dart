9b671fe34d8053edcddc732a174b7f45bda6aff8
n1ghtbyte <tomasgc@disroot.org>
2023-01-12 14:40:04 +0000

    Can see his children on main screen

--- lib/pages/Parent/ParentMain.dart
+++ lib/pages/Parent/ParentMain.dart
@@ -1,4 +1,5 @@
 import 'package:assessments_app/InovWidgets/NavBarParent.dart';
+import 'package:assessments_app/pages/Parent/Student/ChildClasses.dart';
 import 'package:assessments_app/pages/Student/Classes/StudentClassInside.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -14,33 +15,31 @@ class ParentMainScreen extends StatefulWidget {
 class _ParentMainScreenState extends State<ParentMainScreen> {
   String? currentUser = FirebaseAuth.instance.currentUser!.email;
 
-  final _classesStream = FirebaseFirestore.instance
-      .collection('classes')
-      .where('StudentList',
-          arrayContains: FirebaseAuth.instance.currentUser!.email)
-      .snapshots();
+  final _classesStream = FirebaseFirestore.instance.collection('users');
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _classesStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(currentUser).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
         if (snapshot.hasError) {
-          return Text('Something went wrong');
+          return Text("Something went wrong");
         }
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Container(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
         }
-        if (snapshot.data?.size.toInt() == 0) {
+
+        Map? data = snapshot.data?.data() as Map?;
+
+        if (data!['children'] == null) {
           return Scaffold(
             drawer: NavBarParent(),
             appBar: AppBar(
-              title: Text('Classes'),
+              title: Text('Children'),
               centerTitle: true,
               backgroundColor: Color(0xFF29D09E),
             ),
@@ -58,58 +57,42 @@ class _ParentMainScreenState extends State<ParentMainScreen> {
             // ),
             body: Center(
               child: Text(
-                "Classes will be displayed here once your children join any",
+                "You have no official children",
                 style: TextStyle(fontStyle: FontStyle.italic),
                 textAlign: TextAlign.center,
               ),
             ),
           );
+        } else {
+          return Scaffold(
+            drawer: NavBarParent(),
+            appBar: AppBar(
+              title: Text('Children'),
+              centerTitle: true,
+              backgroundColor: Color(0xFF29D09E),
+            ),
+            body: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: data['children'].length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${data['children'][index]}"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChildClasses(
+                          passedEmail: data['children'][index].toString(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          );
         }
-        return Scaffold(
-          drawer: NavBarParent(),
-          appBar: AppBar(
-            title: Text('Classes'),
-            centerTitle: true,
-            backgroundColor: Color(0xFF29D09E),
-          ),
-          // floatingActionButton: FloatingActionButton(
-          //   child: Icon(Icons.add),
-          //   backgroundColor: Color(0xFF29D09E),
-          //   onPressed: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(
-          //         builder: (context) => StudentAddClass(),
-          //       ),
-          //     );
-          //   },
-          // ),
-          body: ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data()! as Map<String, dynamic>;
-              return ListTile(
-                onTap: () {
-                  print(data);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => StudentClassInside(
-                            passedClassId: data['documentID'].toString(),
-                            passedClassName: data['Name'].toString(),
-                            passedEmail: currentUser.toString(),
-                            passedCompetences: data['Competences'])),
-                  );
-                },
-                leading: Icon(Icons.school),
-                isThreeLine: true,
-                title: Text(data['Name']),
-                subtitle: Text(
-                    "${data['NumStudents'].toString()} / ${data['MaxStudents'].toString()}\nHash: ${data['documentID'].toString()}"),
-              );
-            }).toList(),
-          ),
-        );
       },
     );
   }
