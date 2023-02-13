3280825eeb6b32e1ac32f0c3897473899847486d
n1ghtbyte <tomasgc@disroot.org>
2023-02-13 16:55:48 +0000

    gc

--- lib/pages/Teacher/Assessments/AssessmentsMain.dart
+++ lib/pages/Teacher/Assessments/AssessmentsMain.dart
@@ -38,129 +38,6 @@ class _AssessmentsMainState extends State<AssessmentsMain> {
         }
         if (snapshot.data?.size.toInt() == 0) {
           return Scaffold(
-            // drawer: Drawer(
-            //   child: ListView(
-            //     padding: EdgeInsets.zero,
-            //     children: [
-            //       UserAccountsDrawerHeader(
-            //         accountName: Text('Teacher'),
-            //         accountEmail: Text(currentUser!),
-            //         currentAccountPicture: CircleAvatar(
-            //           child: ClipOval(
-            //             child: Image.network(
-            //               // text.data!,
-            //               'https://www.cvexpres.com/teaching-jobs-schools/wp-content/uploads/2021/03/convocatoria-bolsa-docente-maestros-infantil-y-primaria-2.jpg',
-            //               width: 100,
-            //               height: 100,
-            //               fit: BoxFit.cover,
-            //             ),
-            //           ),
-            //         ),
-            //         decoration: BoxDecoration(color: Color(0xFF29D09E)),
-            //       ),
-            //       ListTile(
-            //         leading: Icon(Icons.school),
-            //         title: const Text('Classes'),
-            //         onTap: () {
-            //           Navigator.pop(context);
-            //           Navigator.push(
-            //             context,
-            //             MaterialPageRoute(builder: (context) => ClassesPage()),
-            //           );
-            //         },
-            //       ),
-            //       ListTile(
-            //         leading: Icon(Icons.menu_book),
-            //         title: const Text('Competences'),
-            //         onTap: () {
-            //           Navigator.pop(context);
-
-            //           Navigator.push(
-            //             context,
-            //             MaterialPageRoute(builder: (context) => SkillsPage()),
-            //           );
-            //         },
-            //       ),
-            //       const SizedBox(height: 16),
-            //       Divider(
-            //         thickness: 1,
-            //         height: 1,
-            //       ),
-            //       const SizedBox(height: 16),
-            //       ListTile(
-            //         leading: Icon(Icons.account_box),
-            //         title: const Text('Account'),
-            //         onTap: () {
-            //           Navigator.pop(context);
-
-            //           Navigator.push(
-            //             context,
-            //             MaterialPageRoute(
-            //                 builder: (context) => TeacherProfile()),
-            //           );
-            //         },
-            //       ),
-            //       ListTile(
-            //         leading: Icon(Icons.settings),
-            //         title: const Text('Settings'),
-            //         onTap: () {
-            //           Navigator.push(
-            //             context,
-            //             MaterialPageRoute(builder: (context) => SettingsPage()),
-            //           );
-            //         },
-            //       ),
-            //       ListTile(
-            //         leading: Icon(Icons.logout),
-            //         title: const Text('Log out'),
-            //         onTap: () {
-            //           showDialog(
-            //             context: context,
-            //             builder: (_) => AlertDialog(
-            //               title: Text("Logout"),
-            //               content: Text('Log out now?'),
-            //               actions: [
-            //                 TextButton(
-            //                   style: ButtonStyle(
-            //                     foregroundColor:
-            //                         MaterialStateProperty.all<Color>(
-            //                             Colors.blue),
-            //                   ),
-            //                   onPressed: () {
-            //                     Navigator.of(context, rootNavigator: true)
-            //                         .pop();
-            //                   },
-            //                   child: Text('CANCEL'),
-            //                 ),
-            //                 TextButton(
-            //                   style: ButtonStyle(
-            //                     foregroundColor:
-            //                         MaterialStateProperty.all<Color>(
-            //                             Colors.blue),
-            //                   ),
-            //                   onPressed: () async {
-            //                     await _firebaseAuth.signOut().then(
-            //                       (user) {
-            //                         Navigator.of(context, rootNavigator: true)
-            //                             .pop();
-            //                         Navigator.of(context).pushReplacement(
-            //                           MaterialPageRoute(
-            //                             builder: (context) => LoginScreen(),
-            //                           ),
-            //                         );
-            //                       },
-            //                     );
-            //                   },
-            //                   child: Text('LOGOUT'),
-            //                 ),
-            //               ],
-            //             ),
-            //           );
-            //         },
-            //       ),
-            //     ],
-            //   ),
-            // ),
             appBar: AppBar(
               title: Text('Assessments'),
               centerTitle: true,
@@ -211,7 +88,7 @@ class _AssessmentsMainState extends State<AssessmentsMain> {
                   textColor: data['DONE'] == false
                       ? Color(0xFF29D09E)
                       : Color.fromARGB(255, 123, 123, 123),
-                  title: Text('${data['Type']} Assessment'),
+                  title: Text('${data['Name']}'),
                   subtitle: data['Target'].toString() == 'Single'
                       ? Text(
                           "Class Name:${data['ClassName'].toString()}\nStudent:${data['Students'].keys.toList()[0].toString()}\nDate: ${DateFormat('yyyy-MM-dd').format((data['Created'] as Timestamp).toDate())}")
