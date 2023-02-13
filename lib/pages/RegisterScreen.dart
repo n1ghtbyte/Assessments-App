0428364c9764307a032cdf6e30c73a260286d878
n1ghtbyte <tomasgc@disroot.org>
2023-02-13 16:54:45 +0000

    new register style and +projectassess.eu

--- lib/pages/RegisterScreen.dart
+++ lib/pages/RegisterScreen.dart
@@ -95,227 +95,240 @@ class _RegisterScreenState extends State<RegisterScreen> {
         child: SingleChildScrollView(
           child: Form(
             key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 44.0,
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "Registration Page",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 44.0,
                   ),
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
-                  child: TextFormField(
-                    controller: _firstName,
-                    decoration: InputDecoration(
-                        hintText: "First Name", icon: Icon(Icons.person)),
+                  SizedBox(
+                    height: 44.0,
                   ),
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
-                  child: TextFormField(
-                    controller: _lastName,
-                    decoration: InputDecoration(
-                      hintText: "Last Name",
-                      icon: Icon(Icons.person),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
-                  child: TextFormField(
-                    validator: (val) => val!.isEmpty || !val.contains("@")
-                        ? "enter a valid email"
-                        : null,
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: "Enter you email or account name",
-                      icon: Icon(Icons.email),
-                    ),
-                    // ,
-                  ),
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
-                  child: TextFormField(
-                    controller: _passwordController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      icon: Icon(Icons.lock),
-                    ),
-                    // decoration: buildInputDecoration(Icons.lock, "Password"),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please a Enter Password';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
-                  child: TextFormField(
-                    controller: _passwordController2,
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      hintText: "Confirm Password",
-                      icon: Icon(Icons.lock),
-                    ),
-                    // decoration:
-                    //     buildInputDecoration(Icons.lock, "Confirm Password"),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please re-enter password';
-                      }
-                      print(_passwordController.text);
-                      print(_passwordController2.text);
-                      if (_passwordController.text !=
-                          _passwordController2.text) {
-                        return "Password does not match";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 44.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Radio(
-                      value: 1,
-                      groupValue: id,
-                      onChanged: (val) {
-                        setState(() {
-                          radioButtonItem = 'Teacher';
-                          id = 1;
-                        });
-                      },
-                    ),
-                    Text(
-                      'Teacher',
-                      style: new TextStyle(fontSize: 16),
-                    ),
-                    Radio(
-                      value: 2,
-                      groupValue: id,
-                      onChanged: (val) {
-                        setState(() {
-                          radioButtonItem = 'Student';
-                          id = 2;
-                        });
-                      },
-                    ),
-                    Text(
-                      'Student',
-                      style: new TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    Radio(
-                      value: 3,
-                      groupValue: id,
-                      onChanged: (val) {
-                        setState(() {
-                          radioButtonItem = 'Parent';
-                          id = 3;
-                        });
-                      },
-                    ),
-                    Text(
-                      'Parent',
-                      style: new TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: ElevatedButton(
-                    // elevation: 0.0,
-                    // padding: EdgeInsets.symmetric(vertical: 12.0),
-                    // shape: RoundedRectangleBorder(
-                    //   borderRadius: BorderRadius.circular(12.0),
-                    // ),
-                    onPressed: () async {
-                      if (_passwordController.text ==
-                          _passwordController2.text) {
-                        try {
-                          await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                                  email: _emailController.text,
-                                  password: _passwordController.text);
-                          addUser();
-                          Navigator.of(context).pop();
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password') {
-                            print('The password provided is too weak.');
-                            final snackBar = SnackBar(
-                                content: Text(
-                                    'The password provided is too weak. (min 8 chars)'));
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                          } else if (e.code == 'email-already-in-use') {
-                            print('email-already-in-use');
-                            final snackBar = SnackBar(
-                                content: Text('The account already exists!!'));
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                          }
-                        } catch (e) {
-                          print(e);
-                        }
-                      } else {
-                        final snackBar =
-                            SnackBar(content: Text('Passwords do not match'));
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      }
-                    },
+                  Padding(
+                    padding: EdgeInsets.all(10),
                     child: Text(
-                      "Register",
-                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      "Registration Page",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 44.0,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                    child: TextFormField(
+                      controller: _firstName,
+                      decoration: InputDecoration(
+                          hintText: "First Name",
+                          prefixIcon: Icon(Icons.person)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                    child: TextFormField(
+                      controller: _lastName,
+                      decoration: InputDecoration(
+                        hintText: "Last Name",
+                        prefixIcon: Icon(Icons.person),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                    child: TextFormField(
+                      validator: (val) => val!.isEmpty || !val.contains("@")
+                          ? "enter a valid email"
+                          : null,
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Enter you email or account name",
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      // ,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      // decoration: buildInputDecoration(Icons.lock, "Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please a Enter Password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                    child: TextFormField(
+                      controller: _passwordController2,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: "Confirm Password",
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      // decoration:
+                      //     buildInputDecoration(Icons.lock, "Confirm Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please re-enter password';
+                        }
+                        print(_passwordController.text);
+                        print(_passwordController2.text);
+                        if (_passwordController.text !=
+                            _passwordController2.text) {
+                          return "Password does not match";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Radio(
+                        value: 1,
+                        groupValue: id,
+                        onChanged: (val) {
+                          setState(() {
+                            radioButtonItem = 'Teacher';
+                            id = 1;
+                          });
+                        },
+                      ),
+                      Text(
+                        'Teacher',
+                        style: new TextStyle(fontSize: 16),
+                      ),
+                      Radio(
+                        value: 2,
+                        groupValue: id,
+                        onChanged: (val) {
+                          setState(() {
+                            radioButtonItem = 'Student';
+                            id = 2;
+                          });
+                        },
+                      ),
+                      Text(
+                        'Student',
+                        style: new TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Radio(
+                        value: 3,
+                        groupValue: id,
+                        onChanged: (val) {
+                          setState(() {
+                            radioButtonItem = 'Parent';
+                            id = 3;
+                          });
+                        },
+                      ),
+                      Text(
+                        'Parent',
+                        style: new TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: RawMaterialButton(
+                      fillColor: Color(0xFF29D09E),
+                      elevation: 0.0,
+                      padding: EdgeInsets.symmetric(vertical: 20.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      onPressed: () async {
+                        if (_passwordController.text ==
+                            _passwordController2.text) {
+                          try {
+                            if (!_emailController.text.contains("@")) {
+                              _emailController.text =
+                                  _emailController.text + "@projectassess.eu";
+                            }
+                            await FirebaseAuth.instance
+                                .createUserWithEmailAndPassword(
+                                    email: _emailController.text,
+                                    password: _passwordController.text);
+                            addUser();
+                            Navigator.of(context).pop();
+                          } on FirebaseAuthException catch (e) {
+                            if (e.code == 'weak-password') {
+                              print('The password provided is too weak.');
+                              final snackBar = SnackBar(
+                                  content: Text(
+                                      'The password provided is too weak. (min 8 chars)'));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            } else if (e.code == 'email-already-in-use') {
+                              print('email-already-in-use');
+                              final snackBar = SnackBar(
+                                  content:
+                                      Text('The account already exists!!'));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        } else {
+                          final snackBar =
+                              SnackBar(content: Text('Passwords do not match'));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      },
+                      child: Text(
+                        "Register",
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
