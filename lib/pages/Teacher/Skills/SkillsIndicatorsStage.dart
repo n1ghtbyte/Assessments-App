3da273c9af9f9a36d4179617ea4d188fcc4b7cd0
n1ghtbyte <tomasgc@disroot.org>
2023-02-13 17:00:50 +0000

    Nature does not like internet.

--- lib/pages/Teacher/Skills/SkillsIndicatorsStage.dart
+++ lib/pages/Teacher/Skills/SkillsIndicatorsStage.dart
@@ -1,304 +1,116 @@
-import 'package:assessments_app/pages/Teacher/Skills/Indicator_Settings.dart';
-import 'package:assessments_app/pages/Teacher/Skills/SkillPreview.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SkillsIndicatorsStage extends StatefulWidget {
-  SkillsIndicatorsStage({Key? key}) : super(key: key);
+  final double passedNumIndicator;
+  final String passedCompetenceName;
+
+  const SkillsIndicatorsStage({
+    Key? key,
+    required this.passedNumIndicator,
+    required this.passedCompetenceName,
+  }) : super(key: key);
   @override
   _SkillsIndicatorsStage createState() => _SkillsIndicatorsStage();
 }
 
 class _SkillsIndicatorsStage extends State<SkillsIndicatorsStage> {
+  List<List<TextEditingController>> _descriptionComp = List.generate(
+      5, (index) => List.generate(6, (index) => TextEditingController()));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Indicators Setup'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text('Indicators Creation'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF29D09E),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
           backgroundColor: Color(0xFF29D09E),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: const Color(0xFF29D09E),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SkillPreview()),
-            );
-            // Respond to button press
-          },
-          icon: Icon(Icons.preview),
-          label: Text('Preview'),
-        ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            const SizedBox(height: 16),
-            ListTile(
-              leading: Icon(Icons.drag_indicator),
-              title: Text("1st indicator"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => IndicatorSettings()),
-                );
-              },
-            ),
-            const SizedBox(height: 16),
-            Divider(
-              thickness: 1,
-              height: 1,
-            ),
-            const SizedBox(height: 16),
-            ListTile(
-              leading: Icon(Icons.drag_indicator),
-              title: Text("2nd Indicator"),
-            ),
-
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.group),
-            //   title: Text("Sumative Assessment"),
-            //   subtitle: Text("Deliver Date: XX/YY/ZZ\n Class Turma Exemplo"),
-            //   isThreeLine: true,
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.language),
-            //   title: Text("Languages"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.self_improvement),
-            //   title: Text("Self Assessment"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.school),
-            //   title: Text("Teacher Assessment"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.group),
-            //   title: Text("Peer Assessment"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.self_improvement),
-            //   title: Text("Self Assessment"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.functions),
-            //   title: Text("Math"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.group),
-            //   title: Text("Peer Assessment"),
-            //   subtitle: Text("Deliver Date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.science),
-            //   title: Text("Science"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.group),
-            //   title: Text("Peer Assessment"),
-            //   subtitle: Text("Deliver Date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.language),
-            //   title: Text("Languages"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-            // const SizedBox(height: 16),
-            // Divider(
-            //   thickness: 1,
-            //   height: 1,
-            // ),
-            // const SizedBox(height: 16),
-            // ListTile(
-            //   leading: Icon(Icons.self_improvement),
-            //   title: Text("Self Assessment"),
-            //   subtitle: Text("Deliver date: XX/YY/ZZ"),
-            // ),
-          ],
-        ));
+            Map<String, dynamic> data = Map();
+            data['Name'] = widget.passedCompetenceName;
+            for (var i = 0; i < widget.passedNumIndicator; i++) {
+              for (var j = 1; j < 6; j++) {
+                if (data[_descriptionComp[i][0].text.toString()] == null) {
+                  data[_descriptionComp[i][0].text.toString()] = [
+                    "$j - " + _descriptionComp[i][j].text.toString()
+                  ];
+                } else {
+                  data[_descriptionComp[i][0].text.toString()]
+                      .add("$j - " + _descriptionComp[i][j].text.toString());
+                }
+              }
+            }
+            print(data);
+            FirebaseFirestore.instance
+                .collection("users")
+                .doc(FirebaseAuth.instance.currentUser!.email)
+                .collection("PrivateCompetences")
+                .doc(widget.passedCompetenceName.toString())
+                .set(data)
+                .onError((e, _) => print("Error writing document: $e"));
+          }),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Column(
+            children: [
+              for (var ind = 0; ind < widget.passedNumIndicator; ind++)
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      controller: _descriptionComp[ind][0],
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.comment),
+                        labelText: 'Indicator ${ind + 1}',
+                        labelStyle: TextStyle(
+                          color: Color(0xFF29D09E),
+                        ),
+                        helperText: 'Indicator ${ind + 1} name',
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF29D09E)),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 30, top: 0, right: 0, bottom: 15),
+                      child: ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: 5,
+                        itemBuilder: (context, index2) {
+                          return TextFormField(
+                            controller: _descriptionComp[ind][index2 + 1],
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.comment),
+                              labelText: 'Descriptor ${index2 + 1}',
+                              labelStyle: TextStyle(
+                                color: Color(0xFF29D09E),
+                              ),
+                              helperText:
+                                  'Describe asserting a ${index2 + 1} score',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFF29D09E)),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       drawer: NavBar(),
-//       appBar: AppBar(
-//         title: Text('Main Page'),
-//         centerTitle: true,
-//       ),
-//       body: Center(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-//   final String title;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final theme = Theme.of(context);
-//     final textTheme = theme.textTheme;
-
-//     return Scaffold(
-//       appBar: AppBar(title: Text(title)),
-//       body: const Center(
-//         child: Text('Home Page Feed'),
-//       ),
-//       drawer: Drawer(
-//         child: ListView(
-//           padding: EdgeInsets.all(20.0),
-//           children: <Widget>[
-//             Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Text(
-//                 'Dashboard',
-//                 style: textTheme.headline6
-//               ),
-//             ),
-//             Divider(
-//               height: 1,
-//               thickness: 1,
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.assessment),
-//               title: const Text('Assessments'),
-//               onTap: () {
-//                 // Update the state of the app
-//                 // ...
-//                 // Then close the drawer
-//                 Navigator.pop(context);
-//               },
-//             ),
-
-//             ListTile(
-//               leading: Icon(Icons.text_snippet),
-//               title: const Text('Evaluations'),
-//               onTap: () {
-//                 // Update the state of the app
-//                 // ...
-//                 // Then close the drawer
-//                 Navigator.pop(context);
-//               },
-//             ),
-
-//              ListTile(
-//               leading: Icon(Icons.account_box),
-//               title: const Text('Account'),
-//               onTap: () {
-//                 // Update the state of the app
-//                 // ...
-//                 // Then close the drawer
-//                 Navigator.pop(context);
-//               },
-
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.settings),
-//               title: const Text('Settings'),
-//               onTap: () {
-//                 // Update the state of the app
-//                 // ...
-//                 // Then close the drawer
-//                 Navigator.pop(context);
-//               },
-
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.logout),
-//               title: const Text('Log out'),
-//               onTap: () {
-//                 // Update the state of the app
-//                 // ...
-//                 // Then close the drawer
-//                 Navigator.pop(context);
-//               },
-
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-
-//   }
-// }
--- lib/pages/Teacher/Skills/SkillsPage.dart
+++ lib/pages/Teacher/Skills/SkillsPage.dart
@@ -1,54 +1,130 @@
 import 'package:assessments_app/pages/Teacher/Skills/CompetencePicked.dart';
+import 'package:assessments_app/pages/Teacher/Skills/SkillsCreatePage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SkillsPage extends StatelessWidget {
+class SkillsPage extends StatefulWidget {
+  @override
+  State<SkillsPage> createState() => _SkillsPageState();
+}
+
+class _SkillsPageState extends State<SkillsPage> {
   final CollectionReference _competences =
       FirebaseFirestore.instance.collection('Competences');
+
+  String? currentUser = FirebaseAuth.instance.currentUser!.email;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
-        future: _competences.get(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text("Something went wrong");
-          }
-          if (!snapshot.hasData) {
-            return Container(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-          print(snapshot.data);
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Competences'),
-              centerTitle: true,
-              backgroundColor: Color(0xFF29D09E),
-            ),
-            body: ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                    document.data()! as Map<String, dynamic>;
-                return ListTile(
-                  visualDensity: VisualDensity.standard,
-                  onTap: () {
-                    print(data['Name']);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CompetencePicked(
-                              passedCompName: data['Name'].toString())),
-                    );
-                  },
-                  trailing: Icon(Icons.arrow_circle_right_rounded),
-                  title: Text(data['Name']),
-                );
-              }).toList(),
+      future: _competences.get(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+        if (!snapshot.hasData) {
+          return Container(
+            child: Center(
+              child: CircularProgressIndicator(),
             ),
           );
-        });
+        }
+        return StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(currentUser)
+              .collection('PrivateCompetences')
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshotprivate) {
+            if (snapshotprivate.hasError) {
+              return Text("Something went wrong");
+            }
+            if (!snapshotprivate.hasData) {
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Competences'),
+                centerTitle: true,
+                backgroundColor: Color(0xFF29D09E),
+              ),
+              floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.add),
+                backgroundColor: Color(0xFF29D09E),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SkillsCreatePage(),
+                    ),
+                  );
+                },
+              ),
+              body: SafeArea(
+                child: ListView(
+                  children: snapshot.data!.docs.map(
+                        (DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data()! as Map<String, dynamic>;
+                          return ListTile(
+                            visualDensity: VisualDensity.standard,
+                            onTap: () {
+                              print(data);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CompetencePicked(
+                                    passedComp: data,
+                                    name: data['Name'],
+                                  ),
+                                ),
+                              );
+                            },
+                            trailing: Icon(Icons.arrow_circle_right_rounded),
+                            title: Text(
+                              data['Name'],
+                            ),
+                          );
+                        },
+                      ).toList() +
+                      snapshotprivate.data!.docs.map(
+                        (DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data()! as Map<String, dynamic>;
+                          return ListTile(
+                            visualDensity: VisualDensity.standard,
+                            onTap: () {
+                              print(data);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CompetencePicked(
+                                    passedComp: data,
+                                    name: data['Name'],
+                                  ),
+                                ),
+                              );
+                            },
+                            trailing: Icon(Icons.arrow_circle_right_rounded),
+                            title: Text(
+                              data['Name'],
+                            ),
+                          );
+                        },
+                      ).toList(),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
