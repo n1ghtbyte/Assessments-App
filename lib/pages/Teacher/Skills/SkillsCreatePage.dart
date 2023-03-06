f17ce00247cc505cb50afd17eb44443bc8849bbe
n1ghtbyte <tomasgc@disroot.org>
2023-03-06 16:24:10 +0000

    fix(validator):  orces to populate the form field. Before going to the next screen.

--- lib/pages/Teacher/Skills/SkillsCreatePage.dart
+++ lib/pages/Teacher/Skills/SkillsCreatePage.dart
@@ -12,6 +12,13 @@ class _SkillsCreatePageState extends State<SkillsCreatePage> {
   double _sliderDiscreteValue = 2;
   final _competenceName = TextEditingController();
 
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,57 +30,77 @@ class _SkillsCreatePageState extends State<SkillsCreatePage> {
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: const Color(0xFF29D09E),
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => SkillsIndicatorsStage(
-                      passedCompetenceName: _competenceName.text,
-                      passedNumIndicator: _sliderDiscreteValue,
-                    )),
-          );
+          // Validate returns true if the form is valid, or false otherwise.
+          if (_formKey.currentState!.validate()) {
+            // If the form is valid, display a snackbar. In the real world,
+            // you'd often call a server or save the information in a database.
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => SkillsIndicatorsStage(
+                        passedCompetenceName: _competenceName.text,
+                        passedNumIndicator: _sliderDiscreteValue,
+                      )),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Enter the competence name')),
+            );
+          }
+
           // Respond to button press
         },
         icon: Icon(Icons.skip_next),
         label: Text('Next'),
       ),
-      body: Column(children: [
-        Container(
-          padding: EdgeInsets.all(20),
-          child: TextFormField(
-            maxLength: 20,
-            controller: _competenceName,
-            decoration: InputDecoration(
-              icon: Icon(Icons.comment),
-              labelText: 'Competence name',
-              labelStyle: TextStyle(
-                color: Color(0xFF29D09E),
-              ),
-              helperText: 'What is the name of the competence?',
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFF29D09E)),
+      body: Form(
+        key: _formKey,
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+              maxLength: 20,
+              controller: _competenceName,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                icon: Icon(Icons.comment),
+                labelText: 'Competence name',
+                labelStyle: TextStyle(
+                  color: Color(0xFF29D09E),
+                ),
+                helperText: 'What is the name of the competence?',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF29D09E)),
+                ),
               ),
             ),
           ),
-        ),
-        Divider(),
-        const SizedBox(height: 16),
-        ListTile(
-          leading: Icon(Icons.format_list_numbered),
-          title: Text("How many indicators?"),
-        ),
-        Slider(
-          value: _sliderDiscreteValue,
-          min: 1,
-          max: 5,
-          divisions: 5,
-          label: _sliderDiscreteValue.round().toString(),
-          onChanged: (value) {
-            setState(() {
-              _sliderDiscreteValue = value;
-            });
-          },
-        )
-      ]),
+          Divider(),
+          const SizedBox(height: 16),
+          ListTile(
+            leading: Icon(Icons.format_list_numbered),
+            title: Text("How many indicators?"),
+          ),
+          Slider(
+            value: _sliderDiscreteValue,
+            min: 1,
+            max: 5,
+            divisions: 5,
+            label: _sliderDiscreteValue.round().toString(),
+            onChanged: (value) {
+              setState(() {
+                _sliderDiscreteValue = value;
+              });
+            },
+          )
+        ]),
+      ),
     );
   }
 }
