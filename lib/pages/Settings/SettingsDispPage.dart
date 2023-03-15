08aa9f9178b966beef987c0da35c7c30c78a5876
n1ghtbyte <tomasgc@disroot.org>
2023-03-15 14:39:39 +0000

    refactor: better appearence.

--- lib/pages/Settings/SettingsDispPage.dart
+++ lib/pages/Settings/SettingsDispPage.dart
@@ -8,21 +8,22 @@ class SettingsDispPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Display'),
-        centerTitle: true,
+        centerTitle: false,
         backgroundColor: Color(0xFF29D09E),
       ),
       body: ListView(
         scrollDirection: Axis.vertical,
         children: <Widget>[
-          const SizedBox(height: 16),
-          SwitchListTile(
-            title: Text("\tEnable dark theme"),
-            subtitle:
-                Text("\tSave power"),
-            value: false,
-            onChanged: (val) {},
-            contentPadding: EdgeInsets.all(0),
-            activeColor: Colors.cyan,
+          Container(
+            padding: EdgeInsets.all(5),
+            alignment: Alignment.center,
+            child: SwitchListTile(
+              title: Text("\tEnable dark theme"),
+              subtitle: Text("\tSave power"),
+              value: false,
+              onChanged: (val) {},
+              activeColor: Colors.green,
+            ),
           ),
         ],
       ),
