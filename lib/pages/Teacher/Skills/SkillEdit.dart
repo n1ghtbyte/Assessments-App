e70b0488eca8136849f75f73eb8edd91b82992a2
Tomás Carvalho <tomas@thetimesweeper.com>
2023-04-27 17:55:36 +0100

    refactor: created -> edited

--- lib/pages/Teacher/Skills/SkillEdit.dart
+++ lib/pages/Teacher/Skills/SkillEdit.dart
@@ -66,7 +66,7 @@ class _SkillEditState extends State<SkillEdit> {
             // If the form is valid, display a snackbar. In the real world,
             // you'd often call a server or save the information in a database.
             ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Competence created')),
+              const SnackBar(content: Text('Competence edited')),
             );
             Map<String, dynamic> data = Map();
             data['Name'] = _descriptionComp[0][0].text.toString();
