3d680431e5653bba8859c3231e3ce27e62618538
Tomás Carvalho <tomas@thetimesweeper.com>
2023-11-26 20:26:30 +0000

    feat(pdf): add dates to student progression chart.

--- lib/pages/Teacher/Classes/AstaReport.dart
+++ lib/pages/Teacher/Classes/AstaReport.dart
@@ -1,10 +1,27 @@
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:assessments_app/InovWidgets/ChartData.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
+
+int genColourInt(String competence) {
+  var hash = 0;
+  for (var i = 0; i < competence.length; i++) {
+    hash = competence.codeUnitAt(i) + ((hash << 5) - hash);
+  }
+  final finalHash = hash.abs() % (256 * 256 * 256);
+  print(finalHash);
+  final red = ((finalHash & 0xFF0000) >> 16);
+  final blue = ((finalHash & 0xFF00) >> 8);
+  final green = ((finalHash & 0xFF));
+  final color = Color.fromRGBO(red, green, blue, 1);
+
+  return color.value;
+}
 
 Future<Uint8List> generateReport(
     Map<String, List<DataItem>> _bigData,
@@ -24,7 +41,11 @@ Future<Uint8List> generateReport(
   );
 
   Map<String, List<List<Object>>> dataBarChart = {};
+  Map<String, List<List<Object>>> dataLinesChart = {};
+
   List<pw.Chart> charts = [];
+
+  List<Timestamp> _times = [];
 
   // PRECISA DE MIN 2 INDICADORES !!!
   print("-----------------------------------------------");
@@ -35,6 +56,20 @@ Future<Uint8List> generateReport(
     dataBarChart[comp] = [
       [" ", 0.0]
     ];
+    dataLinesChart[comp] = [
+      [" ", 0.0]
+    ];
+    for (var dt in _smallData[comp]!) {
+      dataLinesChart[comp]?.add([
+        dt.index,
+        dt.value,
+        DateFormat('dd/MM/yyyy').format(dt.timestampDate.toDate()),
+      ]);
+      if (!_times.contains(dt.timestampDate)) {
+        _times.add(dt.timestampDate);
+      }
+    }
+
     for (var dt in _bigData[comp]!) {
       var listInts = [];
 
@@ -47,6 +82,7 @@ Future<Uint8List> generateReport(
       dataBarChart[comp]?.add([dt.x, avg]);
     }
   }
+
   for (var comp in _bigData.keys) {
     if (dataBarChart[comp]!.length > 2) {
       dataBarChart[comp]!.removeAt(0);
@@ -54,6 +90,17 @@ Future<Uint8List> generateReport(
       dataBarChart[comp]!.add(["", 0.0]);
     }
   }
+  for (var comp in _bigData.keys) {
+    if (dataLinesChart[comp]!.length > 2) {
+      dataLinesChart[comp]!.removeAt(0);
+    } else {
+      dataLinesChart[comp]!.add(["", 0.0]);
+    }
+  }
+
+  List<String> formattedDates = _times.map((timestamp) {
+    return DateFormat('dd/MM/yyyy').format(timestamp.toDate());
+  }).toList();
 
   print(dataBarChart);
 
@@ -88,6 +135,43 @@ Future<Uint8List> generateReport(
     cellAlignments: {0: pw.Alignment.centerLeft},
   );
   print("-----------------------------------------------");
+
+  final chart2 = pw.Chart(
+    // title: pw.Text(AppLocalizations.of(contextLanguage)!.studentprog),
+    overlay: pw.ChartLegend(),
+    grid: pw.CartesianGrid(
+      xAxis: pw.FixedAxis.fromStrings(
+        List<String>.generate(
+            (formattedDates.length).toInt(), (index) => formattedDates[index]),
+        marginStart: 0,
+        marginEnd: 30,
+        ticks: true,
+      ),
+      yAxis: pw.FixedAxis(
+        [0, 1, 2, 3, 4, 5],
+        divisions: true,
+      ),
+    ),
+    datasets: [
+      for (var x in _smallData.keys)
+        pw.LineDataSet(
+          lineWidth: 3,
+          legend: x,
+          drawSurface: false,
+          isCurved: true,
+          drawPoints: true,
+          color: PdfColor.fromInt(genColourInt(x)),
+          data: List<pw.PointChartValue>.generate(
+            dataLinesChart[x]!.length,
+            (i) {
+              final v = dataLinesChart[x]![i][1] as num;
+              final y = dataLinesChart[x]![i][0] as num;
+              return pw.PointChartValue(y.toDouble(), v.toDouble());
+            },
+          ),
+        ),
+    ],
+  );
 
   // Top bar chart
   for (var comp in dataBarChart.keys) {
@@ -151,68 +235,68 @@ Future<Uint8List> generateReport(
         // Page layout
         return pw.Column(
           children: [
-            pw.Row(
-              crossAxisAlignment: pw.CrossAxisAlignment.start,
-              children: [
-                pw.Expanded(
-                  child: pw.Column(
-                    children: [
-                      pw.Container(
-                        height: 150,
-                        padding: const pw.EdgeInsets.only(left: 20),
-                        alignment: pw.Alignment.centerLeft,
-                        child: pw.Text(
-                          'Assessments App Report',
-                          style: pw.TextStyle(
-                            color: baseColor,
-                            fontWeight: pw.FontWeight.bold,
-                            fontSize: 40,
-                          ),
-                        ),
+            pw.Expanded(
+              flex: 0,
+              child: pw.Column(
+                children: [
+                  pw.Container(
+                    height: 150,
+                    padding: const pw.EdgeInsets.only(left: 20),
+                    alignment: pw.Alignment.centerLeft,
+                    child: pw.Text(
+                      'Assessments App Report',
+                      style: pw.TextStyle(
+                        color: baseColor,
+                        fontWeight: pw.FontWeight.bold,
+                        fontSize: 40,
                       ),
-                      pw.Container(
-                        decoration: pw.BoxDecoration(
-                          borderRadius:
-                              const pw.BorderRadius.all(pw.Radius.circular(2)),
-                        ),
-                        alignment: pw.Alignment.centerLeft,
-                        height: 50,
-                        child: pw.DefaultTextStyle(
-                          style: pw.TextStyle(
-                            fontSize: 12,
-                          ),
-                          child: pw.GridView(
-                            crossAxisCount: 2,
-                            children: [
-                              pw.Text('Date:'),
-                              pw.Text(DateFormat.yMMMEd()
-                                  .format(DateTime.now())
-                                  .toString()),
-                              pw.Text('Class:'),
-                              pw.Text(info[0]),
-                              pw.Text('Student:'),
-                              pw.Text(info[1]),
-                            ],
-                          ),
-                        ),
-                      ),
-                      pw.SizedBox(height: 20),
-                      pw.Divider(),
-                      pw.Text(
-                        "Summative assessments",
-                        style: pw.TextStyle(
-                          color: baseColor,
-                          fontWeight: pw.FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      pw.SizedBox(height: 10),
-                      table
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                  pw.Container(
+                    decoration: pw.BoxDecoration(
+                      borderRadius:
+                          const pw.BorderRadius.all(pw.Radius.circular(2)),
+                    ),
+                    alignment: pw.Alignment.centerLeft,
+                    height: 50,
+                    child: pw.DefaultTextStyle(
+                      style: pw.TextStyle(
+                        fontSize: 12,
+                      ),
+                      child: pw.GridView(
+                        crossAxisCount: 2,
+                        children: [
+                          pw.Text('Date:'),
+                          pw.Text(DateFormat.yMMMEd()
+                              .format(DateTime.now())
+                              .toString()),
+                          pw.Text('Class:'),
+                          pw.Text(info[0]),
+                          pw.Text('Student:'),
+                          pw.Text(info[1]),
+                        ],
+                      ),
+                    ),
+                  ),
+                  pw.SizedBox(height: 20),
+                  pw.Divider(),
+                  pw.Text(
+                    "Summative assessments",
+                    style: pw.TextStyle(
+                      color: baseColor,
+                      fontWeight: pw.FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  pw.SizedBox(height: 10),
+                  table,
+                ],
+              ),
             ),
+            pw.SizedBox(height: 20),
+            pw.Divider(height: 5),
+            pw.SizedBox(height: 40),
+            pw.Expanded(flex: 1, child: chart2),
             if (context.pageNumber > 1) pw.SizedBox(height: 20)
           ],
         );
