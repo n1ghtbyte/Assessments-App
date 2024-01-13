81cb148058eade7ea890846e7ecebd72aef4b21b
Tomás Carvalho <tomas@thetimesweeper.com>
2024-01-13 16:08:54 +0000

    fix scrolling

--- lib/pages/Teacher/Classes/ClassSetup.dart
+++ lib/pages/Teacher/Classes/ClassSetup.dart
@@ -31,114 +31,119 @@ class _ClassSetupState extends State<ClassSetup> {
 
   Widget build(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
-        future: _class.doc(widget.passedClassNameSetup).get(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text("Something went wrong");
-          }
+      future: _class.doc(widget.passedClassNameSetup).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
 
-          if (!snapshot.hasData) {
-            return Container(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-          print("lolo");
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
-
-          var comp = data['Competences'];
-          List<String?> list = [];
-          comp.entries.forEach((e) => list.add(e.key));
-          var numComp = list.length;
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(AppLocalizations.of(context)!.setupweights),
-              centerTitle: true,
-              backgroundColor: Color(0xFF29D09E),
+        if (!snapshot.hasData) {
+          return Container(
+            child: Center(
+              child: CircularProgressIndicator(),
             ),
-            floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.check),
-              onPressed: () {
-                Map<String?, int> map1 = {};
-                int sum = 0;
+          );
+        }
+        Map<String, dynamic> data =
+            snapshot.data!.data() as Map<String, dynamic>;
+
+        var comp = data['Competences'];
+        List<String?> list = [];
+        comp.entries.forEach((e) => list.add(e.key));
+        var numComp = list.length;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(AppLocalizations.of(context)!.setupweights),
+            centerTitle: true,
+            backgroundColor: Color(0xFF29D09E),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.check),
+            onPressed: () {
+              Map<String?, int> map1 = {};
+              int sum = 0;
+              for (int i = 0; i < numComp; i++) {
+                sum += int.parse(_controllers[i].text);
+              }
+              if (sum == 100) {
                 for (int i = 0; i < numComp; i++) {
-                  sum += int.parse(_controllers[i].text);
+                  map1[list[i].toString()] = int.parse(_controllers[i].text);
                 }
-                if (sum == 100) {
-                  for (int i = 0; i < numComp; i++) {
-                    map1[list[i].toString()] = int.parse(_controllers[i].text);
-                  }
-                  FirebaseFirestore.instance
-                      .collection("/classes")
-                      .doc(widget.passedClassNameSetup)
-                      .set({"Weights": map1}, SetOptions(merge: true));
-                  final snackBar = SnackBar(
-                      content: Text(AppLocalizations.of(context)!.valsstored));
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                FirebaseFirestore.instance
+                    .collection("/classes")
+                    .doc(widget.passedClassNameSetup)
+                    .set({"Weights": map1}, SetOptions(merge: true));
+                final snackBar = SnackBar(
+                    content: Text(AppLocalizations.of(context)!.valsstored));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
 
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              TurmaExemplo(widget.passedClassNameSetup)));
-                  print("success");
-                } else {
-                  final snackBar = SnackBar(
-                      content: Text(AppLocalizations.of(context)!.summhundred));
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              },
-              backgroundColor: Color(0xFF29D09E),
-            ),
-            body: Column(
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            TurmaExemplo(widget.passedClassNameSetup)));
+                print("success");
+              } else {
+                final snackBar = SnackBar(
+                    content: Text(AppLocalizations.of(context)!.summhundred));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+            },
+            backgroundColor: Color(0xFF29D09E),
+          ),
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
               children: [
                 Container(
                   padding: EdgeInsets.all(20),
                   child: Text(
                     AppLocalizations.of(context)!.attrweigh,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
                     textAlign: TextAlign.center,
                   ),
                 ),
                 ListView.builder(
-                    shrinkWrap: true,
-                    physics: ScrollPhysics(),
-                    itemCount: numComp,
-                    itemBuilder: (BuildContext context, int index) {
-                      _controllers.add(new TextEditingController());
-                      return Container(
-                        padding: EdgeInsets.only(left: 50, right: 50),
-                        child: TextFormField(
-                          onChanged: (value) {
-                            total = "0";
-                            total = _calculTotal(_controllers);
-                            setState(() {});
-                          },
-                          keyboardType: TextInputType.number,
-                          controller: _controllers[index],
-                          decoration: InputDecoration(
-                            labelText: list[index],
-                            labelStyle: TextStyle(
-                              color: Color(0xFF29D09E),
-                            ),
-                            counterText: "%",
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF29D09E)),
-                            ),
+                  shrinkWrap: true,
+                  physics: ScrollPhysics(),
+                  itemCount: numComp,
+                  itemBuilder: (BuildContext context, int index) {
+                    _controllers.add(new TextEditingController());
+                    return Container(
+                      padding: EdgeInsets.only(left: 50, right: 50),
+                      child: TextFormField(
+                        onChanged: (value) {
+                          total = "0";
+                          total = _calculTotal(_controllers);
+                          setState(() {});
+                        },
+                        keyboardType: TextInputType.number,
+                        controller: _controllers[index],
+                        decoration: InputDecoration(
+                          labelText: list[index],
+                          labelStyle: TextStyle(
+                            color: Color(0xFF29D09E),
+                          ),
+                          counterText: "%",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF29D09E)),
                           ),
                         ),
-                      );
-                    }),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
                 Wrap(
                   alignment: WrapAlignment.spaceAround, // set your alignment
                   children: <Widget>[
                     Text("Total: ",
                         style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600)),
+                            fontSize: 16, fontWeight: FontWeight.w400)),
                     Text(
                       total,
                       style: TextStyle(
@@ -149,15 +154,22 @@ class _ClassSetupState extends State<ClassSetup> {
                             : Colors.green,
                       ),
                     ),
-                    Text(" %",
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600)),
+                    Text(
+                      " %",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    ),
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
               ],
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
 
